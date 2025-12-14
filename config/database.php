349877cb40769d2eac2f3a3a544832b4708caea3
Techyishu <shashanksingh67567@gmail.com>
2025-12-14 19:47:58 +0530

    Configure Wasmer for MySQL: Remove PostgreSQL config and add Wasmer config files

--- config/database.php
+++ config/database.php
@@ -40,20 +40,6 @@ return [
                 return $options;
             })(),
         ],
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'feeadmin'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
     ],
     'migrations' => [
         'table' => 'migrations',
